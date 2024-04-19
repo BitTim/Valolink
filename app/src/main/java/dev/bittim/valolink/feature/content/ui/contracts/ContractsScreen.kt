518103122ff43c47df96484b122e248101d4d01d
Tim Anhalt <tim@anhalt.me>
2024-04-20 01:29:04 +0200

    Added icon button for later use to open "Show all" Dialog

--- app/src/main/java/dev/bittim/valolink/feature/content/ui/contracts/ContractsScreen.kt
+++ app/src/main/java/dev/bittim/valolink/feature/content/ui/contracts/ContractsScreen.kt
@@ -3,6 +3,7 @@ package dev.bittim.valolink.feature.content.ui.contracts
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,7 +12,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowForward
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SegmentedButton
@@ -26,6 +31,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import dev.bittim.valolink.feature.content.domain.model.agent.Agent
@@ -105,32 +111,33 @@ fun ContractsScreen(
             Spacer(modifier = Modifier.height(8.dp))
         }
 
+        // TODO: Implement a proper fullscreen dialog instead
         stickyHeader {
             Surface(
                 modifier = Modifier
                     .fillMaxWidth()
                     .wrapContentHeight()
             ) {
-                Text(
+                Row(
                     modifier = Modifier.padding(bottom = 4.dp),
-                    text = "Agents",
-                    style = MaterialTheme.typography.headlineMedium
-                )
+                    horizontalArrangement = Arrangement.spacedBy(4.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = "Agents", style = MaterialTheme.typography.headlineMedium
+                    )
+
+                    IconButton(onClick = { }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Default.ArrowForward,
+                            contentDescription = null
+                        )
+                    }
+                }
             }
         }
 
         item {
-            val random = Random()
-            var isLockedList by remember { mutableStateOf(listOf<Boolean>()) }
-            var unlockedLevelsList by remember { mutableStateOf(listOf<Int>()) }
-            if (isLockedList.isEmpty()) {
-                isLockedList = List(state.agentGears.count()) { _ -> random.nextBoolean() }
-            }
-            if (unlockedLevelsList.isEmpty()) {
-                unlockedLevelsList =
-                    List(state.agentGears.count()) { index -> random.nextInt(state.agentGears[index].calcLevelCount()) }
-            }
-
             HorizontalMultiBrowseCarousel(
                 state = state.agentGearCarouselState,
                 preferredItemWidth = 300.dp,
@@ -138,6 +145,18 @@ fun ContractsScreen(
                 flingBehavior = CarouselDefaults.multiBrowseFlingBehavior(state = state.agentGearCarouselState)
             ) { index ->
                 val gear = state.agentGears[index]
+
+                // TODO: Random values for testing
+                val random = Random()
+                var isLockedList by remember { mutableStateOf(listOf<Boolean>()) }
+                var unlockedLevelsList by remember { mutableStateOf(listOf<Int>()) }
+                if (isLockedList.isEmpty()) {
+                    isLockedList = List(state.agentGears.count()) { _ -> random.nextBoolean() }
+                }
+                if (unlockedLevelsList.isEmpty()) {
+                    unlockedLevelsList =
+                        List(state.agentGears.count()) { i -> random.nextInt(state.agentGears[i].calcLevelCount()) }
+                }
 
                 if (gear.content.relation is Agent) {
                     AgentCarouselCard(
