0f6653f2b05d535bd3821063fe3ca057159e12c5
Tim Anhalt <tim@anhalt.me>
2026-06-23 04:18:22 +0200

    Update get method in `ValoRankTableDao` to use `Uuid` instead of `String` refs BIT-67
    - Modified the `get()` method parameter type from `String` to `Uuid`.
    - Imported `kotlin.uuid.Uuid` to support the updated parameter type.

--- mobile/shared/src/commonMain/kotlin/dev/bittim/valolink/core/data/local/dao/ValoRankTableDao.kt
+++ mobile/shared/src/commonMain/kotlin/dev/bittim/valolink/core/data/local/dao/ValoRankTableDao.kt
@@ -7,7 +7,7 @@
  * File:       ValoRankTableDao.kt
  * Module:     Valolink.shared.commonMain
  * Author:     Tim Anhalt (BitTim)
- * Modified:   23.06.26, 03:01
+ * Modified:   23.06.26, 04:18
  */
 
 package dev.bittim.valolink.core.data.local.dao
@@ -17,6 +17,7 @@ import androidx.room.Query
 import androidx.room.Upsert
 import dev.bittim.valolink.core.data.local.entity.ValoRankTableEntity
 import kotlinx.coroutines.flow.Flow
+import kotlin.uuid.Uuid
 
 @Dao
 interface ValoRankTableDao {
@@ -34,7 +35,7 @@ interface ValoRankTableDao {
      * @return A Flow emitting the matching `ValoRankTableEntity`, or `null` if no entity exists for the given UUID.
      */
     @Query("SELECT * FROM valo_rank_tables WHERE uuid = :uuid")
-    fun get(uuid: String): Flow<ValoRankTableEntity?>
+    fun get(uuid: Uuid): Flow<ValoRankTableEntity?>
 
     /**
      * Retrieves all rank table entities.
