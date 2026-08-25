72af4203bc34334679fe2a3fb74d7421dd0af265
Tim Anhalt <tim@anhalt.me>
2026-08-25 22:03:59 +0200

    Refactor `insertParticipant` logic in `MatchRepo` and `SupabaseMatchRepo` (refs BIT-77)
    - Replaced `MatchParticipantDraft` with `MatchParticipant` in `insertParticipant` method signature.
    - Updated `SupabaseMatchRepo` to use `MatchParticipantDto` for model mapping.
    - Aligned domain interface (`MatchRepo`) with repository implementation changes.
    - Removed references to `MatchParticipantDraft` to streamline participant handling across components.
    
    # Conflicts:
    #	mobile/shared/src/commonMain/kotlin/dev/bittim/valolink/core/data/repo/SupabaseMatchRepo.kt
    #	mobile/shared/src/commonMain/kotlin/dev/bittim/valolink/core/domain/repo/MatchRepo.kt

--- mobile/shared/src/commonMain/kotlin/dev/bittim/valolink/core/data/repo/SupabaseMatchRepo.kt
+++ mobile/shared/src/commonMain/kotlin/dev/bittim/valolink/core/data/repo/SupabaseMatchRepo.kt
@@ -7,7 +7,7 @@
  * File:       SupabaseMatchRepo.kt
  * Module:     Valolink.shared.commonMain
  * Author:     Tim Anhalt (BitTim)
- * Modified:   25.08.26, 22:13
+ * Modified:   25.08.26, 21:20
  */
 
 package dev.bittim.valolink.core.data.repo
--- mobile/shared/src/commonMain/kotlin/dev/bittim/valolink/core/domain/repo/MatchRepo.kt
+++ mobile/shared/src/commonMain/kotlin/dev/bittim/valolink/core/domain/repo/MatchRepo.kt
@@ -7,7 +7,7 @@
  * File:       MatchRepo.kt
  * Module:     Valolink.shared.commonMain
  * Author:     Tim Anhalt (BitTim)
- * Modified:   25.08.26, 22:13
+ * Modified:   25.08.26, 21:15
  */
 
 package dev.bittim.valolink.core.domain.repo
